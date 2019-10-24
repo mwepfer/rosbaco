711f1bf465178873de056299d75f068428f224eb
Matias Wepfer <mwepfer@gmail.com>
2019-10-24 12:17:07 -0300

    Commit matias

--- src/aura/ProductCatalogTabs/ProductCatalogTabsController.js
+++
@@ -1,8 +0,0 @@
-({
-    createFamilies: function(component, event, helper){
-    	console.log("Help");
-        var products = event.getParam("products");
-        console.log("PRODUCTS IN TABS CONTROLLER",products);
-        component.set("v.products",products);
-    },
-})
